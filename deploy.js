ffa5bc1ae1bdf837745ae6cd0abdc2812a6d708a
BulutKaradag <40030779+BulutKaradag@users.noreply.github.com>
2022-11-29 18:08:19 +0300

    Add files via upload

--- deploy.js
+++ deploy.js
@@ -1,20 +1,20 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+ 
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider('verb gun sell field pudding welcome habit lizard pattern park unfold awful','https://goerli.infura.io/v3/e2ba16136286497eb6fd4a5ace3b51a0');
 const web3 = new Web3(provider);
-
+ 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-
+ 
   console.log('Attempting to deploy from account', accounts[0]);
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
     .send({ gas: '1000000', from: accounts[0] });
-
+ 
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
-deploy();
